ffbc9e4527c8848bbe5da3e0a21846c23854993e
Kourosh Asadi <Kouroshasadi244@gmail.com>
2025-08-01 19:04:12 +0330

    House .py

--- House .py
+++ House .py
@@ -4,42 +4,47 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, r2_score
 
 
-# reading the main data 
+# Reading the main data 
 
-df=pd.read_csv("F:\DATA\/House.csv")
+df=pd.read_csv("F:\DATA\House\/House.csv")
 
 
-# cleaning the data 
-# address 
+# Cleaning the data 
+ 
 df = df[df['Address'].notna() & (df['Address'] != "")]
 # Area
 df['Area'] = pd.to_numeric(df['Area'], errors='coerce')
 df = df[df['Area'] <= 900]
-
-#____________________________________________________________
 
 # turning bollean datas to numbers 
 
 df['Parking'] = df['Parking'].astype(int)
 df['Warehouse'] = df['Warehouse'].astype(int)
 df['Elevator'] = df['Elevator'].astype(int)
+df = pd.get_dummies(df, columns=['Address'], drop_first=True)
 
-# normalize the address
+df.to_csv('House_cleaned.csv', index=False)
+
+
+
+# Normalize
 
 df = pd.get_dummies(df, columns=['Address'], drop_first=True)
-# area,room,address,parking,warehouse,elavotor are correct    
+
+
 df.to_csv('House_cleaned.csv', index=False)
 #__________________________________________________________
 
-x = df.drop(['Price', 'Price(USD)'], axis=1)  
-y = df['Price(USD)']  
+# Target x,y 
+x = df.drop(['Price', 'Price(USD)'], axis=1)          
+y = df['Price(USD)']                                  
 
 # normalize the data
 
-x = x / x.max()
-y = y / y.max()
+x = x / x.max()                    # or   x = preprocessing.StandardScaler().fit(x).transform(x.astype(float))
+y = y / y.max()                 
 
-# devide the data to 90% for train and 10% for the test
+# Train & test
 
 msk = np.random.rand(len(df)) < 0.9
 train_x = x[msk]
@@ -48,13 +53,12 @@ train_y = y[msk]
 test_y = y[~msk]
 
 
-# random forrest
+# Modeling
 
-regr = RandomForestRegressor(n_estimators=100, random_state=42)          # 100 , 42 are Usual
-regr.fit(train_x, train_y)
+regr = RandomForestRegressor(n_estimators=100, random_state=42).fit(train_x,train_y)         
 
 
-# test the data
+# Test
 
 test_y_ = regr.predict(test_x)
 
@@ -65,6 +69,5 @@ r2 = r2_score(test_y, test_y_)
 print('Mean Absolute Error:',MAE)
 print('MSE:',MSE)
 print('R2:',r2)
-
 
 
